5c01b39a619e043add41dbcb39a780d5d7752599
PawanK <tutor_pawan@hotmail.com>
2023-02-07 16:12:08 +0700

    add footer in every page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,12 @@
 import React from "react";
+import ContactFooter from "./ContactFooter";
+import SeoFooter from "./SeoFooter";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="hook-wraper">
-        <div>
-          <h2>Find Best Car For Renting</h2>
-          <p>
-            Find the best car of your choice using different searching option.
-          </p>
-          <button>Contac Us</button>
-        </div>
-      </div>
-      <div className="seo-footer">hah</div>
+      <ContactFooter />
+      <SeoFooter />
     </div>
   );
 };
