e5a35a58869782115fea7ef6b9a185a242af1618
Kantaponn <78014536+Kantaponn@users.noreply.github.com>
2023-04-01 19:03:33 +0700

    add pay now, rate booking button to my booking

--- src/components/MyBooking.jsx
+++ src/components/MyBooking.jsx
@@ -63,6 +63,29 @@ const MyBooking = () => {
     fetchMyBooking();
   };
 
+  const purchaseBooking = async (car_id, match_id) => {
+    try {
+      // await axios.patch(
+      //   `http://localhost:8080/match/cancel-reservation`,
+      //   {},
+      //   {
+      //     headers: {
+      //       car_id: car_id,
+      //       match_id: match_id,
+      //     },
+      //     withCredentials: true,
+      //   }
+      // );
+    } catch (error) {
+      console.log(error);
+    }
+    fetchMyBooking();
+  };
+
+  const rateBooking = async (car_id, match_id) => {
+    window.location.assign(`/addReview/?matchID=${match_id}`);
+  };
+
   useEffect(() => {
     fetchMyBooking();
     // setIsLoading(false);
@@ -107,6 +130,7 @@ const MyBooking = () => {
               returnDateTime,
               returnLocation,
               status,
+              isReview,
             } = match;
             const pickupDate = new Date(pickUpDateTime);
             const returnDate = new Date(returnDateTime);
@@ -133,12 +157,30 @@ const MyBooking = () => {
                   <p>{`Pickup : ${pickupDate.toLocaleString()} at ${pickupLocation}`}</p>
                   <p>{`Return : ${returnDate.toLocaleString()} at ${returnLocation}`}</p>
                   <div className="footer">
-                    {status === "Pending" && (
+                    {(status === "Unverified renter" ||
+                      status === "Wait for payment") && (
                       <h3
-                        className="cancel"
+                        className="cancel btn"
                         onClick={() => cancelBooking(car_id, match_id)}
                       >
                         ✖ Cancel booking
+                      </h3>
+                    )}
+                    {status === "Wait for payment" && (
+                      <h3
+                        className="pay btn"
+                        onClick={() => purchaseBooking(car_id, match_id)}
+                      >
+                        ✓ Pay now
+                      </h3>
+                    )}
+                    {isReview}
+                    {status === "Completed" && isReview == false && (
+                      <h3
+                        className="rate btn"
+                        onClick={() => rateBooking(car_id, match_id)}
+                      >
+                        <i class="fa-regular fa-comment"></i> Rate booking
                       </h3>
                     )}
                     <h3 className="price">{`${calculatePrice(
